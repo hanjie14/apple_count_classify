7588bb2a441a48cfb24a5f2ba7bca9949af97bcd
hanjie14 <hanjeecheong@gmail.com>
2023-08-02 11:45:22 +0800

    v1.0

--- development/README.md
+++ development/README.md
@@ -2,3 +2,8 @@
 
 pip install -r requirement.txt
 
+# Files required
+
+1. place the two images provided in the Input folder
+2. download pretrained model yolov3.pt into the Models folder
+
--- development/classify.py
+++ development/classify.py
@@ -2,6 +2,7 @@ from imageai.Detection import ObjectDetection
 from pathlib import Path
 import cv2
 import numpy as np
+import os
 
 def check_folder(folder_path_list):
     if isinstance(folder_path_list, list) :
@@ -10,20 +11,18 @@ def check_folder(folder_path_list):
             if not folder_path.is_dir():
                 folder_path.mkdir(parents=True)
 
-def find_next_filename(filename):
+def find_next_filename(filename, output_dir):
     name, ext = os.path.splitext(filename)
     i = 1
     while os.path.exists(os.path.join(output_dir, filename)):
         i += 1
         filename = f"{name}_{i}{ext}"
-    return filename
+    return os.path.join(output_dir, filename)
 
 
 def find_dominant_color(image):
-    # Convert the image to the HSV color space
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     
-    # Define color ranges for red, green, and yellow
     red_lower = np.array([0, 120, 70])
     red_upper = np.array([10, 255, 255])
     
@@ -33,17 +32,14 @@ def find_dominant_color(image):
     yellow_lower = np.array([20, 100, 100])
     yellow_upper = np.array([40, 255, 255])
     
-    # Create masks for each color
     red_mask = cv2.inRange(hsv_image, red_lower, red_upper)
     green_mask = cv2.inRange(hsv_image, green_lower, green_upper)
     yellow_mask = cv2.inRange(hsv_image, yellow_lower, yellow_upper)
     
-    # Count the number of pixels in each mask
     red_pixels = cv2.countNonZero(red_mask)
     green_pixels = cv2.countNonZero(green_mask)
     yellow_pixels = cv2.countNonZero(yellow_mask)
     
-    # Find the dominant color
     max_pixels = max(red_pixels, green_pixels, yellow_pixels)
     if max_pixels == red_pixels:
         return "red"
@@ -75,7 +71,6 @@ def main(model_path, input_image, output_image,minimum_percentage_probability):
                                                                                      extract_detected_objects=True, 
                                                                                      minimum_percentage_probability=minimum_percentage_probability)
 
-    print("The number "+ str(len(detections))+ " is the number of apples that are in the image.")
-    print("extracted_objects")
-    # print("Object detection complete!")
+    # print("The number "+ str(len(detections))+ " is the number of apples that are in the image.")
+    # print("extracted_objects")
     return returned_image, detections, extracted_objects
